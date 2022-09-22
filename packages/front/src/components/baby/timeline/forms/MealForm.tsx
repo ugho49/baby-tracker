af9b8eabe18048f58921d17576efc1bc64456338
Ugho STEPHAN <ugho.stephan@payfit.com>
2022-09-22 15:46:50 +0200

    fix(front): add meal max length

--- packages/front/src/components/baby/timeline/forms/MealForm.tsx
+++ packages/front/src/components/baby/timeline/forms/MealForm.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Meal } from '@baby-tracker/common-types';
 import { Stack, TextField } from '@mui/material';
 import { FormProps } from '../BabyTimelineComponentForms';
-import { DateTime } from 'luxon';
-import { MobileTimePicker } from '@mui/x-date-pickers';
 import { isBlank } from '../../../../utils';
+
+const MEAL_MAX_LENGTH = 60;
 
 export const MealForm = ({ onStateChange, onValidChange, disabled, initialState }: FormProps<Meal>) => {
   const [meal, setMeal] = useState<string | undefined>(initialState?.meal || '');
   const [note, setNote] = useState<string | undefined>(initialState?.note);
 
+  const mealMaxLengthExceeded = useMemo(() => meal !== undefined && meal.length > MEAL_MAX_LENGTH, [meal]);
+
   useEffect(() => {
     const state: Meal = { meal: meal || '', note: note === '' ? undefined : note };
     onStateChange(state);
-    onValidChange(!isBlank(meal));
-  }, [onStateChange, onValidChange, meal, note]);
+    onValidChange(!isBlank(meal) && !mealMaxLengthExceeded);
+  }, [onStateChange, onValidChange, meal, note, mealMaxLengthExceeded]);
 
   return (
     <Stack sx={{ display: 'flex', gap: 2 }}>
@@ -23,7 +25,8 @@ export const MealForm = ({ onStateChange, onValidChange, disabled, initialState 
         disabled={disabled}
         fullWidth
         value={meal}
-        error={meal !== undefined && meal.length > 60}
+        error={mealMaxLengthExceeded}
+        helperText={mealMaxLengthExceeded && `${MEAL_MAX_LENGTH} Caractères maximum`}
         placeholder="Ex: Purée carotte, petit pot, yaourt etc ..."
         onChange={(e) => setMeal(e.target.value)}
       />
