2c3c0b58869468042362c808eb24bd0bc48cc6ed
Ugho STEPHAN <ugho.stephan@payfit.com>
2022-09-22 09:39:58 +0200

    feat: upgrade nx version

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { DomainModule } from './domain/domain.module';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [CoreModule, DomainModule],
